7fef4c8a7faa0207dc5615cc9991cf677b9a08fb
esaricoglu <84877085+esaricoglu@users.noreply.github.com>
2023-01-25 12:23:31 +0300

    Add files via upload

--- SayiSiralama.java
+++ SayiSiralama.java
@@ -24,16 +24,16 @@ public class SayiSiralama {
                 System.out.println(a + "," + c + "," + b);
             }
         } else if (b <= a && b <= c) {
-            if (a<c){
-                System.out.println(b+","+a+","+c);
-            }else{
-                System.out.println(b+","+c+","+a);
+            if (a < c) {
+                System.out.println(b + "," + a + "," + c);
+            } else {
+                System.out.println(b + "," + c + "," + a);
             }
-        }else if(c<=a && c<=b){
-            if(a<b){
-                System.out.println(c+","+a+","+b);
-            }else{
-                System.out.println(c+","+b+","+a);
+        } else if (c <= a && c <= b) {
+            if (a < b) {
+                System.out.println(c + "," + a + "," + b);
+            } else {
+                System.out.println(c + "," + b + "," + a);
             }
         }
     }
